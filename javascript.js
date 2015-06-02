51427b744c525f24ab8ed750425ec017906427af
Bank <bankyalexander@gmail.com>
2015-06-02 05:58:14 +0000

    add all files

---
+++ javascript.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+	$('#button1').click(function () {
+		$('#logo1').attr('src', 'Q:\INFOSYS.280\Assignment2_Tue_01\bale084\LabTest-280\images\turnoff.png');
+		$('#logo2').attr('src', 'Q:\INFOSYS.280\Assignment2_Tue_01\bale084\LabTest-280\images\turnoff.png'); 
+	});
+});
